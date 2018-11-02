69b843a50097e700bf9876c87091839f75488e87
dhumlicek <dhumlicek@gmail.com>
2018-11-02 13:59:47 -0500

    Dice program

--- dice01.py
+++ dice01.py
@@ -3,8 +3,24 @@ from random import randint
 x = 10
 n = 0
 while n < x:
-    r=randint(0,6)
-    C='o '
-    s='-----\n|'+C[r<1]+' '+C[r<3]+'|\n|'+C[r<5]
-    print(s+C[r&1]+s[::-1])
+    r_num = randint(0,5)    # converts to 1-6
+    dot ='o '
+    s = '-----\n|' + dot[r_num < 1] + ' ' + dot[r_num < 3] + '|\n|' + dot[r_num < 5]
+    print(s + dot[r_num&1] + s[::-1])
+
+    #print('+' * 10)
+    #print(r_num)
+
+    # if FALSE=first char; if TRUE=second char
+    #print(r_num < 1)
+    #print(r_num < 3)
+    #print(r_num < 5)
+    #print(dot[r_num < 1])
+    #print(dot[r_num < 3])
+    #print(dot[r_num < 5])
+
+    #print('print string: \n%s' % s)
+    #print('print middle dot: %s' % dot[r_num&1])
+    #print('print reverse string: \n%s' % s[::-1])
+
     n += 1
